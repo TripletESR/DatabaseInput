72fa3015fb346ba2a9206df39d3e0d88e635d682
TripletESR-Desktop <ryantang@cns.s.u-tokyo.ac.jp>
2017-04-10 17:06:27 +0900

    set mainwidow always on top

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    Qt::WindowFlags flags = w.windowFlags();
+    w.setWindowFlags(flags | Qt::WindowStaysOnTopHint | Qt::X11BypassWindowManagerHint);
     w.show();
 
     return a.exec();
