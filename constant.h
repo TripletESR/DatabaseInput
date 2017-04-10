6d653276b9ce63c79a4424ee03ce9ba5c9f3f4d7
TripletESR-Desktop <ryantang@cns.s.u-tokyo.ac.jp>
2017-04-10 17:25:17 +0900

    forget to git constant.h in last time

--- constant.h
+++ constant.h
@@ -1,13 +1,36 @@
 #ifndef CONSTANT_H
 #define CONSTANT_H
 
+#include "constant.h"
 #include <QString>
 #include <QStandardPaths>
 
-const QString DESKTOP_PATH = QStandardPaths::locate(QStandardPaths::DesktopLocation, QString(), QStandardPaths::LocateDirectory);
-const QString HOME_PATH = QStandardPaths::locate(QStandardPaths::HomeLocation, QString(), QStandardPaths::LocateDirectory);
-const QString DATA_PATH = HOME_PATH + "ESR_Data_201610/";
-const QString ChemicalPicture_PATH = DATA_PATH + "Chemical_Pic/";
-const QString SamplePicture_PATH = DATA_PATH + "ESR_Sample_Pic/";
-const QString DB_PATH =  DESKTOP_PATH + "/test.db" ;
+// any global variabel has to be defined in constant.h and constant.cpp
+
+extern const QString DESKTOP_PATH ;
+extern const QString HOME_PATH ;
+//extern const QString CONFIG_PATH ;
+
+extern QString DATA_PATH ;
+extern QString CHEMICAL_PIC_PATH ;
+extern QString SAMPLE_PIC_PATH ;
+extern QString HALL_PATH ;
+extern QString LOG_PATH ;
+extern QString DB_PATH ;
+
+extern double TORRGRAD ;
+extern double TIME1 ;
+extern double TIME2 ;
+
 #endif // CONSTANT_H
+
+/**************************
+======== TODO--List =======
+***************************
+1, convert Dialog to mainwindow class ( need edit *.ui file outside Qt )
+2, moving average has bug
+3, better guest on par
+4, calculate error-weighted mean
+5, undecimated DWT
+6, non-orthonormal DWT
+***************************/
